d3cb2052dd5c421ec780ab490248f8855fc81e2b
Mavlon <Mavlon@localhost>
2020-01-12 11:42:46 +0500

    Проверить ДЗ

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,30 @@
-console.log("Its worked!");
-console.log("hello!");
+const REGULAR_CATEGORY =  "regular";
+const INCREASED_CATEGORY = "increased";
+const SPECIAL_CATEGORY = "special";
+
+const purchases = [
+{
+    amont:1000,
+    category: REGULAR_CATEGORY,
+},
+
+{
+    amont:1000,
+    category:INCREASED_CATEGORY,
+},
+
+{
+    amont:3000,
+    category: SPECIAL_CATEGORY,
+},
+];
+
+let cashback = 0;
+let sum = 0;
+
+for (const purchase of purchases) {
+    sum+=purchase.amont;
+    cashback = sum / 100;
+}
+
+console.log(cashback);
